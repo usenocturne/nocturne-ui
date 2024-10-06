3786f52ae13188bb9465eab8e9295efab55f43ec
Brandon Saldan <26472557+brandonsaldan@users.noreply.github.com>
2024-10-06 00:22:58 -0400

    feat: add spotify attribution

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -37,51 +37,68 @@ export default function Settings() {
   }, []);
 
   return (
-    <div className="pt-12 space-y-8">
-      <div>
-        <Field className="flex items-center">
-          <Switch
-            checked={trackNameScrollingEnabled}
-            onChange={setTrackNameScrollingEnabled}
-            className="group relative inline-flex h-11 w-20 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent bg-gray-200 transition-colors duration-200 ease-in-out focus:outline-none data-[checked]:bg-white/40"
+    <div className="h-full overflow-y-auto">
+      <div className="pt-12 space-y-8">
+        <div>
+          <Field className="flex items-center">
+            <Switch
+              checked={trackNameScrollingEnabled}
+              onChange={setTrackNameScrollingEnabled}
+              className="group relative inline-flex h-11 w-20 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent bg-gray-200 transition-colors duration-200 ease-in-out focus:outline-none data-[checked]:bg-white/40"
+            >
+              <span
+                aria-hidden="true"
+                className="pointer-events-none inline-block h-10 w-10 transform rounded-full bg-white shadow ring-0 transition duration-200 ease-in-out group-data-[checked]:translate-x-9"
+              />
+            </Switch>
+            <Label as="span" className="ml-3 text-sm">
+              <span className="text-[32px] font-[580] text-white tracking-tight">
+                Track Name Scrolling
+              </span>
+            </Label>
+          </Field>
+          <p className="pt-4 text-[28px] font-[560] text-white/60 max-w-[380px] tracking-tight">
+            Enable or disable the scrolling animation for the track name in the
+            player.
+          </p>
+        </div>
+        <div>
+          <Field className="flex items-center">
+            <Switch
+              checked={lyricsMenuEnabled}
+              onChange={setLyricsMenuEnabled}
+              className="group relative inline-flex h-11 w-20 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent bg-gray-200 transition-colors duration-200 ease-in-out focus:outline-none data-[checked]:bg-white/40"
+            >
+              <span
+                aria-hidden="true"
+                className="pointer-events-none inline-block h-10 w-10 transform rounded-full bg-white shadow ring-0 transition duration-200 ease-in-out group-data-[checked]:translate-x-9"
+              />
+            </Switch>
+            <Label as="span" className="ml-3 text-sm">
+              <span className="text-[32px] font-[580] text-white tracking-tight">
+                Lyrics Menu Option
+              </span>
+            </Label>
+          </Field>
+          <p className="pt-4 text-[28px] font-[560] text-white/60 max-w-[380px] tracking-tight">
+            Enable or disable the lyrics menu option in the player.
+          </p>
+        </div>
+        <div className="relative">
+          <div
+            aria-hidden="true"
+            className="absolute inset-0 flex items-center"
           >
-            <span
-              aria-hidden="true"
-              className="pointer-events-none inline-block h-10 w-10 transform rounded-full bg-white shadow ring-0 transition duration-200 ease-in-out group-data-[checked]:translate-x-9"
-            />
-          </Switch>
-          <Label as="span" className="ml-3 text-sm">
-            <span className="text-[32px] font-[580] text-white tracking-tight">
-              Track Name Scrolling
-            </span>
-          </Label>
-        </Field>
-        <p className="pt-4 text-[28px] font-[560] text-white/60 max-w-[380px] tracking-tight">
-          Enable or disable the scrolling animation for the track name in the
-          player.
-        </p>
-      </div>
-      <div>
-        <Field className="flex items-center">
-          <Switch
-            checked={lyricsMenuEnabled}
-            onChange={setLyricsMenuEnabled}
-            className="group relative inline-flex h-11 w-20 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent bg-gray-200 transition-colors duration-200 ease-in-out focus:outline-none data-[checked]:bg-white/40"
-          >
-            <span
-              aria-hidden="true"
-              className="pointer-events-none inline-block h-10 w-10 transform rounded-full bg-white shadow ring-0 transition duration-200 ease-in-out group-data-[checked]:translate-x-9"
-            />
-          </Switch>
-          <Label as="span" className="ml-3 text-sm">
-            <span className="text-[32px] font-[580] text-white tracking-tight">
-              Lyrics Menu Option
-            </span>
-          </Label>
-        </Field>
-        <p className="pt-4 text-[28px] font-[560] text-white/60 max-w-[380px] tracking-tight">
-          Enable or disable the lyrics menu option in the player.
-        </p>
+            <div className="w-full border-t border-gray-300" />
+          </div>
+        </div>
+        <div>
+          <p className="pt-4 pb-4 text-[20px] font-[560] text-white/60 max-w-[380px] tracking-tight">
+            All album artwork, artist images, and track metadata are provided by
+            Spotify Technology S.A. These materials are protected by
+            intellectual property rights owned by Spotify or its licensors.
+          </p>
+        </div>
       </div>
     </div>
   );
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -23,131 +23,137 @@ export default function Home({
             />
           </div>
 
-          <div className="flex overflow-x-auto scroll-container p-2">
-            {activeSection === "recents" && (
-              <>
-                {albumsQueue.map((album) => (
-                  <div key={album.id} className="min-w-[280px] mr-10">
+          <div className="h-screen overflow-y-auto">
+            <div className="flex overflow-x-auto scroll-container p-2">
+              {activeSection === "recents" && (
+                <>
+                  {albumsQueue.map((album) => (
+                    <div key={album.id} className="min-w-[280px] mr-10">
+                      <LongPressLink
+                        href={`/album/${album.id}`}
+                        spotifyUrl={album.external_urls.spotify}
+                        accessToken={accessToken}
+                      >
+                        <img
+                          src={album.images[0]?.url}
+                          alt="Currently Playing Album Cover"
+                          className="mt-10 w-[280px] h-[280px] aspect-square rounded-[12px] drop-shadow-xl"
+                        />
+                      </LongPressLink>
+                      <LongPressLink
+                        href={`/album/${album.id}`}
+                        spotifyUrl={album.external_urls.spotify}
+                        accessToken={accessToken}
+                      >
+                        <h4 className="mt-2 text-[36px] font-[580] text-white truncate tracking-tight max-w-[280px]">
+                          {album.name}
+                        </h4>
+                      </LongPressLink>
+                      <LongPressLink
+                        href={`/artist/${album.artists[0].id}`}
+                        spotifyUrl={album.artists[0].external_urls.spotify}
+                        accessToken={accessToken}
+                      >
+                        <h4 className="text-[32px] font-[560] text-white truncate tracking-tight max-w-[280px]">
+                          {album.artists
+                            .map((artist) => artist.name)
+                            .join(", ")}
+                        </h4>
+                      </LongPressLink>
+                    </div>
+                  ))}
+                </>
+              )}
+              {activeSection === "library" &&
+                playlists.map((item) => (
+                  <div key={item.id} className="min-w-[280px] mr-10">
                     <LongPressLink
-                      href={`/album/${album.id}`}
-                      spotifyUrl={album.external_urls.spotify}
+                      href={`/playlist/${item.id}`}
+                      spotifyUrl={item.external_urls.spotify}
                       accessToken={accessToken}
                     >
                       <img
-                        src={album.images[0]?.url}
-                        alt="Currently Playing Album Cover"
+                        src={item.images[0]?.url}
+                        alt="Playlist Cover"
                         className="mt-10 w-[280px] h-[280px] aspect-square rounded-[12px] drop-shadow-xl"
                       />
                     </LongPressLink>
                     <LongPressLink
-                      href={`/album/${album.id}`}
-                      spotifyUrl={album.external_urls.spotify}
+                      href={`/playlist/${item.id}`}
+                      spotifyUrl={item.external_urls.spotify}
                       accessToken={accessToken}
                     >
                       <h4 className="mt-2 text-[36px] font-[580] text-white truncate tracking-tight max-w-[280px]">
-                        {album.name}
+                        {item.name}
                       </h4>
                     </LongPressLink>
-                    <LongPressLink
-                      href={`/artist/${album.artists[0].id}`}
-                      spotifyUrl={album.artists[0].external_urls.spotify}
-                      accessToken={accessToken}
-                    >
-                      <h4 className="text-[32px] font-[560] text-white truncate tracking-tight max-w-[280px]">
-                        {album.artists.map((artist) => artist.name).join(", ")}
-                      </h4>
-                    </LongPressLink>
+                    <h4 className="text-[28px] font-[560] text-white truncate tracking-tight max-w-[280px]">
+                      {item.tracks.total.toLocaleString()} Songs
+                    </h4>
                   </div>
                 ))}
-              </>
-            )}
-            {activeSection === "library" &&
-              playlists.map((item) => (
-                <div key={item.id} className="min-w-[280px] mr-10">
-                  <LongPressLink
-                    href={`/playlist/${item.id}`}
-                    spotifyUrl={item.external_urls.spotify}
-                    accessToken={accessToken}
-                  >
-                    <img
-                      src={item.images[0]?.url}
-                      alt="Playlist Cover"
-                      className="mt-10 w-[280px] h-[280px] aspect-square rounded-[12px] drop-shadow-xl"
-                    />
-                  </LongPressLink>
-                  <LongPressLink
-                    href={`/playlist/${item.id}`}
-                    spotifyUrl={item.external_urls.spotify}
-                    accessToken={accessToken}
-                  >
-                    <h4 className="mt-2 text-[36px] font-[580] text-white truncate tracking-tight max-w-[280px]">
-                      {item.name}
+              {activeSection === "artists" &&
+                artists.map((artist) => (
+                  <div key={artist.id} className="min-w-[280px] mr-10">
+                    <LongPressLink
+                      href={`/artist/${artist.id}`}
+                      spotifyUrl={artist.external_urls.spotify}
+                      accessToken={accessToken}
+                    >
+                      <img
+                        src={artist.images[0]?.url}
+                        alt="Artist"
+                        className="mt-10 w-[280px] h-[280px] aspect-square rounded-full drop-shadow-xl"
+                      />
+                    </LongPressLink>
+                    <LongPressLink
+                      href={`/artist/${artist.id}`}
+                      spotifyUrl={artist.external_urls.spotify}
+                      accessToken={accessToken}
+                    >
+                      <h4 className="mt-2 text-[36px] font-[580] text-white truncate tracking-tight max-w-[280px]">
+                        {artist.name}
+                      </h4>
+                    </LongPressLink>
+                    <h4 className="text-[28px] font-[560] text-white truncate tracking-tight max-w-[280px]">
+                      {artist.followers.total.toLocaleString()} Followers
                     </h4>
-                  </LongPressLink>
-                  <h4 className="text-[28px] font-[560] text-white truncate tracking-tight max-w-[280px]">
-                    {item.tracks.total.toLocaleString()} Songs
-                  </h4>
-                </div>
-              ))}
-            {activeSection === "artists" &&
-              artists.map((artist) => (
-                <div key={artist.id} className="min-w-[280px] mr-10">
-                  <LongPressLink
-                    href={`/artist/${artist.id}`}
-                    spotifyUrl={artist.external_urls.spotify}
-                    accessToken={accessToken}
-                  >
-                    <img
-                      src={artist.images[0]?.url}
-                      alt="Artist"
-                      className="mt-10 w-[280px] h-[280px] aspect-square rounded-full drop-shadow-xl"
-                    />
-                  </LongPressLink>
-                  <LongPressLink
-                    href={`/artist/${artist.id}`}
-                    spotifyUrl={artist.external_urls.spotify}
-                    accessToken={accessToken}
-                  >
-                    <h4 className="mt-2 text-[36px] font-[580] text-white truncate tracking-tight max-w-[280px]">
-                      {artist.name}
+                  </div>
+                ))}
+              {activeSection === "radio" &&
+                radio.map((playlist) => (
+                  <div key={playlist.id} className="min-w-[280px] mr-10">
+                    <LongPressLink
+                      href={`/playlist/${playlist.id}`}
+                      spotifyUrl={playlist.external_urls.spotify}
+                      accessToken={accessToken}
+                    >
+                      <img
+                        src={playlist.images[0]?.url}
+                        alt="Playlist Cover"
+                        className="mt-10 w-[280px] h-[280px] aspect-square rounded-[12px] drop-shadow-xl"
+                      />
+                    </LongPressLink>
+                    <LongPressLink
+                      href={`/playlist/${playlist.id}`}
+                      spotifyUrl={playlist.external_urls.spotify}
+                      accessToken={accessToken}
+                    >
+                      <h4 className="mt-2 text-[36px] font-[580] text-white truncate tracking-tight max-w-[280px]">
+                        {playlist.name}
+                      </h4>
+                    </LongPressLink>
+                    <h4 className="text-[28px] font-[560] text-white truncate tracking-tight max-w-[280px]">
+                      {playlist.owner.display_name}
                     </h4>
-                  </LongPressLink>
-                  <h4 className="text-[28px] font-[560] text-white truncate tracking-tight max-w-[280px]">
-                    {artist.followers.total.toLocaleString()} Followers
-                  </h4>
+                  </div>
+                ))}
+              {activeSection === "settings" && (
+                <div className="w-full h-full overflow-y-auto">
+                  <Settings accessToken={accessToken} />
                 </div>
-              ))}
-            {activeSection === "radio" &&
-              radio.map((playlist) => (
-                <div key={playlist.id} className="min-w-[280px] mr-10">
-                  <LongPressLink
-                    href={`/playlist/${playlist.id}`}
-                    spotifyUrl={playlist.external_urls.spotify}
-                    accessToken={accessToken}
-                  >
-                    <img
-                      src={playlist.images[0]?.url}
-                      alt="Playlist Cover"
-                      className="mt-10 w-[280px] h-[280px] aspect-square rounded-[12px] drop-shadow-xl"
-                    />
-                  </LongPressLink>
-                  <LongPressLink
-                    href={`/playlist/${playlist.id}`}
-                    spotifyUrl={playlist.external_urls.spotify}
-                    accessToken={accessToken}
-                  >
-                    <h4 className="mt-2 text-[36px] font-[580] text-white truncate tracking-tight max-w-[280px]">
-                      {playlist.name}
-                    </h4>
-                  </LongPressLink>
-                  <h4 className="text-[28px] font-[560] text-white truncate tracking-tight max-w-[280px]">
-                    {playlist.owner.display_name}
-                  </h4>
-                </div>
-              ))}
-            {activeSection === "settings" && (
-              <Settings accessToken={accessToken} />
-            )}
+              )}
+            </div>
           </div>
         </div>
       )}
