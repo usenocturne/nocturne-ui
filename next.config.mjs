5b3f13cdf3f1994c8e4abcd48fc92d6a9eb6779d
Brandon Saldan <26472557+brandonsaldan@users.noreply.github.com>
2024-10-28 22:58:20 -0400

    fix: use wildcard hostnames

--- next.config.mjs
+++ next.config.mjs
@@ -8,40 +8,8 @@ const nextConfig = {
         },
         {
           protocol: 'https',
-          hostname: 'i.scdn.co',
+          hostname: '**.spotifycdn.com',
         },
-        {
-          protocol: 'https',
-          hostname: 'mosaic.scdn.co',
-        },
-        {
-          protocol: 'https',
-          hostname: 'daylist.spotifycdn.com',
-        },
-        {
-          protocol: 'https',
-          hostname: 'seed-mix-image.spotifycdn.com',
-        },
-        {
-          protocol: 'https',
-          hostname: 'image-cdn-fa.spotifycdn.com',
-        },
-        {
-          protocol: 'https',
-          hostname: 'image-cdn-ak.spotifycdn.com',
-        },
-        {
-          protocol: 'https',
-          hostname: 'wrapped-images.spotifycdn.com',
-        },
-        {
-          protocol: 'https',
-          hostname: 'pickasso.spotifycdn.com',
-        },
-        {
-          protocol: 'https',
-          hostname: 'blend-playlist-covers.spotifycdn.com',
-        }
       ],
     },
   };
