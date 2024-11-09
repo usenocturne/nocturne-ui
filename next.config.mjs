4895663092112f78c0781a5ed9b9dce57909522e
Brandon Saldan <26472557+brandonsaldan@users.noreply.github.com>
2024-11-08 22:40:33 -0500

    fix: use cloudflare pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,23 @@
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: '**.scdn.co',
-        },
-        {
-          protocol: 'https',
-          hostname: '**.spotifycdn.com',
-        },
-      ],
-    },
-  };
-  
-  export default nextConfig;
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.scdn.co',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.spotifycdn.com',
+      },
+    ],
+  },
+};
+
+if (process.env.NODE_ENV === 'development') {
+  await setupDevPlatform();
+}
+
+export default nextConfig;
