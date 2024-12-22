e527bbca6ce26f6bc74b9592e452bcfdeca94c4d
Brandon Saldan <26472557+brandonsaldan@users.noreply.github.com>
2024-12-21 22:08:23 -0500

    fix(playlists): display playlists in library

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -240,7 +240,10 @@ export default function Home({
               {activeSection === "library" && (
                 <>
                   {likedSongs && (
-                    <div className="min-w-[280px] mr-10 snap-start">
+                    <div
+                      key="liked-songs"
+                      className="min-w-[280px] mr-10 snap-start"
+                    >
                       <LongPressLink
                         href="/collection/tracks"
                         spotifyUrl={likedSongs.external_urls.spotify}
@@ -268,44 +271,43 @@ export default function Home({
                       </h4>
                     </div>
                   )}
-                  {playlists &&
-                    playlists.map((item) =>
-                      item && item.id ? (
-                        <div
-                          key={item.id}
-                          className="min-w-[280px] mr-10 snap-start"
+                  {playlists
+                    ?.filter((item) => item?.type === "playlist")
+                    .map((playlist) => (
+                      <div
+                        key={`playlist-${playlist.id}`}
+                        className="min-w-[280px] mr-10 snap-start"
+                      >
+                        <LongPressLink
+                          href={`/playlist/${playlist.id}`}
+                          spotifyUrl={playlist?.external_urls?.spotify}
+                          accessToken={accessToken}
                         >
-                          <LongPressLink
-                            href={`/playlist/${item.id}`}
-                            spotifyUrl={item?.external_urls?.spotify}
-                            accessToken={accessToken}
-                          >
-                            <Image
-                              src={
-                                item?.images?.[0]?.url ||
-                                "/images/not-playing.webp"
-                              }
-                              alt="Playlist Cover"
-                              width={280}
-                              height={280}
-                              className="mt-10 aspect-square rounded-[12px] drop-shadow-xl"
-                            />
-                          </LongPressLink>
-                          <LongPressLink
-                            href={`/playlist/${item.id}`}
-                            spotifyUrl={item?.external_urls?.spotify}
-                            accessToken={accessToken}
-                          >
-                            <h4 className="mt-2 text-[36px] font-[580] text-white truncate tracking-tight max-w-[280px]">
-                              {item.name}
-                            </h4>
-                          </LongPressLink>
-                          <h4 className="text-[28px] font-[560] text-white truncate tracking-tight max-w-[280px]">
-                            {item.tracks?.total?.toLocaleString() || 0} Songs
+                          <Image
+                            src={
+                              playlist?.images?.[0]?.url ||
+                              "/images/not-playing.webp"
+                            }
+                            alt={`${playlist.name} Cover`}
+                            width={280}
+                            height={280}
+                            className="mt-10 aspect-square rounded-[12px] drop-shadow-xl"
+                          />
+                        </LongPressLink>
+                        <LongPressLink
+                          href={`/playlist/${playlist.id}`}
+                          spotifyUrl={playlist?.external_urls?.spotify}
+                          accessToken={accessToken}
+                        >
+                          <h4 className="mt-2 text-[36px] font-[580] text-white truncate tracking-tight max-w-[280px]">
+                            {playlist.name}
                           </h4>
-                        </div>
-                      ) : null
-                    )}
+                        </LongPressLink>
+                        <h4 className="text-[28px] font-[560] text-white truncate tracking-tight max-w-[280px]">
+                          {playlist.tracks?.total?.toLocaleString() || 0} Songs
+                        </h4>
+                      </div>
+                    ))}
                 </>
               )}
               {activeSection === "artists" &&
