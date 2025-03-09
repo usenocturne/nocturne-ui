42c4d6734ffb16885edd1d1bd2a14957b641e3f9
Brandon Saldan <26472557+brandonsaldan@users.noreply.github.com>
2025-03-09 16:35:17 -0400

    fix: import useRef

--- src/components/tutorial/Tutorial.jsx
+++ src/components/tutorial/Tutorial.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import TutorialFrame from "./TutorialFrame";
 import NocturneIcon from "../common/icons/NocturneIcon";
 import { useGradientState } from "../../hooks/useGradientState";
