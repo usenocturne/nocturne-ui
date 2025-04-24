57fcbbd14db39fcac9e28ffdc912cd6751fd4170
Álvaro González Serna <50526113+alvvaro@users.noreply.github.com>
2025-04-24 19:29:28 +0200

    memoize useGradientState

--- src/hooks/useGradientState.js
+++ src/hooks/useGradientState.js
@@ -1,14 +1,26 @@
+import { useMemo } from 'react';
+import { useCallback } from 'react';
 import { useState } from 'react';
 
 export function useGradientState(activeSection = null) {
   const [imageURL, setImageURL] = useState(null);
   const [section, setSection] = useState(activeSection);
 
-  const gradientState = { imageURL, section };
-  const setGradientState = (newImageURL = null, newSection = null) => {
-    setImageURL(newImageURL);
-    setSection(newSection);
-  };
+  const gradientState = useMemo(
+    () => ({
+      imageURL,
+      section,
+    }),
+    [imageURL, section],
+  );
+
+  const setGradientState = useCallback(
+    (newImageURL = null, newSection = null) => {
+      setImageURL(newImageURL);
+      setSection(newSection);
+    },
+    [],
+  );
 
   return [gradientState, setGradientState];
 }
