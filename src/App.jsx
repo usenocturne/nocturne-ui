c2bd86089d1b0fd2394bd876190483d056bbe96f
shadow <108803410+68p@users.noreply.github.com>
2025-08-01 10:03:58 -0400

    fix: preset button overlay has exit animation again

--- src/App.jsx
+++ src/App.jsx
@@ -1062,7 +1062,7 @@ function App() {
                         onClose={handleNetworkClose}
                         onConnect={handleNetworkClose}
                       />
-                      {!showTutorial && showGlobalMappingOverlay && (
+                      {!showTutorial && (
                         <ButtonMappingOverlay
                           show={showGlobalMappingOverlay}
                           activeButton={globalActiveButton}
